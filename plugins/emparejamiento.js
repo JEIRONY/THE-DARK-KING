7d19e4bd1105fa5a3f70afcfa7d5da7b681ad214
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-03-28 14:07:07 -0500

    Update emparejamiento.js

--- plugins/emparejamiento.js
+++ plugins/emparejamiento.js
@@ -15,7 +15,7 @@ function handler(m, { groupMetadata }) {
 }
 handler.help = ['formarpareja']
 handler.tags = ['main']
-handler.command = ['formarpareja','formarparejas']
+handler.command = ['88']
 handler.group = true
 
 module.exports = handler
