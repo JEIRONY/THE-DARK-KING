385e85989d0c1cbb51291b0b859d6c3e878615d1
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-03-28 21:41:15 -0500

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -2,14 +2,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix }) => {
 let who = m.sender
-let pp = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
-let kai = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
-let text = `*https://github.com/BrunoSobrino/ShadowBotV3*`.trim()
+let pp = await(await fetch('https://i.imgur.com/1eyNT8k.jpg')).buffer()
+let kai = await(await fetch('https://i.imgur.com/1eyNT8k.jpg')).buffer()
+let text = `*https://www.youtube.com/channel/UCN9wxOL23R-6hA2XgupyWZQ*`.trim()
 conn.reply(m.chat, text, m, { thumbnail: kai, contextInfo: {
 mentionedJid: [who],
 externalAdReply: {
 title: '𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃',
-body: '🤖 Ｓｈａｄｏｗ Ｂｏｔ 🤖',
+body: '►─═ ＪＥＩＲＯＮＹ ═─◄',
 thumbnail: pp
 }}})}
 handler.command = /^sc|script|repositorio|repobot|scbot$/i
