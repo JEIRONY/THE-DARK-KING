e9050286bdff68ca317b8adb0cffe69a150e6028
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-03-28 21:10:44 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,9 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '5219992095479', 'Shadow - Creador - Oficial 1', m)
-this.sendContact(m.chat, '17722386341', 'Shadow - Creador - Oficial 2', m)}
+this.sendContact(m.chat, '573152139466', 'JEIRONY - Creador - Oficial 1', m)
+this.sendContact(m.chat, '48459070337', 'JEIRONY- Creador - NUNEROS DE AYUDA ', m)
+this.sendContact(m.chat, '48699530441', 'JEIRONY - Creador - NUNEROS DE AYUDA ', m)
+this.sendContact(m.chat, '48699520550', 'JEIRONY - Creador - NUNEROS DE AYUDA ', m)
+this.sendContact(m.chat, '79266513672', 'JEIRONY - Creador - NUNEROS DE AYUDA ', m)}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
