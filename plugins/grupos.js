355391e04f4dfa8752a0d27860b1b14ec4ab3367
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-03-31 18:54:59 -0500

    Add files via upload

---
+++ plugins/grupos.js
@@ -0,0 +1,16 @@
+let { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  
+let info = `
+*HOLA SOY THE DARK KING AQUI TIENES MIS GRUPOS DE AYUDA*
+
+*➤ Grupos oficiales del Bot:*
+*1.-* https://chat.whatsapp.com/El6hMgOd2SJ3v4LHXjON0l
+
+`.trim() 
+
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE DARK KING 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
+
+handler.command = /^(grupos|gruposofc|gruposofc)$/i
+module.exports = handler
