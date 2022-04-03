2323cbac31fd7877b92b46b50d0a3f7700c32a9d
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:32:05 -0500

    Update futbol.js

--- plugins/futbol.js
+++ plugins/futbol.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/futbol")
 let json = res.data
-let THE DARK KING = json.url
+let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, THE DARK KING, "*Fútbol 🥅*", '©THE DARK KING', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
