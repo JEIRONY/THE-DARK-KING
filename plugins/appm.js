36f1b2d3ad0f214ce8ec9dfb93066f3c8a64e56b
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-02 20:40:05 -0500

    Create appm.js

---
+++ plugins/appm.js
@@ -0,0 +1,27 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+let moment = require('moment-timezone')
+let path = require('path')
+let util = require('util')
+let handler = async (m, { conn, usedPrefix }) => {
+let pp = './minecraft.jpg'
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let username = conn.getName(who)
+let vn = './media/pisadas.mp3'
+let menu =`
+=☆=☆=【MENU MINECRAFT】=☆=☆=
+★━━━━━━━━━━━━━━━━━━━━★
+ *✨𝗛ola, ${username}!!*
+★━━━━━━━━━━━━━━━━━━━━★
+
+◆HOLA MI PANA COMO ESTAS.
+AQUI ABAJO TE DEJO ALGUNOS LINKS DE DESCARGA ESPERO SEA DE TU AGRADO.`.trim()
+let mentionedJid = [who]
+conn.send2ButtonImg(m.chat, pp, menu, '©𝑱𝑬𝑰𝑺𝑶𝑵 - Bot', '𝕄𝔼𝔻𝕀𝔸𝔽𝕀ℝ𝔼', `#descarga`, '𝕄𝔼𝔾𝔸', `#descarga2`, m, false, { contextInfo: { mentionedJid }})   
+await await await await await await conn.sendFile(m.chat, vn, 'ora.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})}
+handler.command = /^(app)$/i
+handler.fail = null
+module.exports = handler
