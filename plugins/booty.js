b9f6bfcd7d9fda3433340aa8f2fc6ff5b325e423
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:30:03 -0500

    Update booty.js

--- plugins/booty.js
+++ plugins/booty.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/booty")
 let json = res.data
-let THE DARK KING = json.url
+let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, THE DARK KING, "*Booty 🍑❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
