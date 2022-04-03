b88b89825f508ee0d12921d47d7284002eb5ec72
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:38:16 -0500

    Update porno2.js

--- plugins/porno2.js
+++ plugins/porno2.js
@@ -2,18 +2,12 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/porngif")
 let json = res.data
-let THE DARK KING = json.url
+let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, THE DARK KING, "*🥵🥵🥵🥵🥵*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(porno2)$/i
-handler.owner = true
-handler.mods = false
-handler.premium = false
 handler.group = true
-handler.private = false
 handler.admin = true
-handler.botAdmin = true
-handler.fail = null
 module.exports = handler
 
