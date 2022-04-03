62b8fca7b3b9a0434169dbdcd12bf383b8eb8796
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:32:41 -0500

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -2,9 +2,11 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/sticker")
 let json = res.data
-let THE DARK KING = json.url
+let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, THE DARK KING, "*Random img 👽*", '©THE DARK KING', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(imagenrandom)$/i
+handler.group = true
+handler.admin = true
 module.exports = handler
