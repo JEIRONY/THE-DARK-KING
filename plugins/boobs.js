05bc5abf788b5020fb812c44e56b0ab86ebf0a83
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:41:21 -0500

    Update boobs.js

--- plugins/boobs.js
+++ plugins/boobs.js
@@ -5,7 +5,7 @@ let json = res.data
 let TheDarking = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*Boobs 🍒❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*Boobs 🍒❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(boobs|tetas)$/i
 handler.group = true
 handler.admin = true
