f6ee56a36e0fce886e7fad8b835956c68968a36a
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:47:11 -0500

    Update navidad.js

--- plugins/navidad.js
+++ plugins/navidad.js
@@ -5,7 +5,7 @@ let json = res.data
 let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*Navidad 🧑‍🎄*", '©THE DARK KING', '🎄 SIGUIENTE 🎄', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*Navidad 🧑‍🎄*", '©THE DARK KING', '🎄 SIGUIENTE 🎄', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(navidad)$/i
 handler.group = true
 handler.admin = true
