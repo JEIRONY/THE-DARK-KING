c2e0c496f9010425142cc48a004768a8ab26d181
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:48:37 -0500

    Update random_meme.js

--- plugins/random_meme.js
+++ plugins/random_meme.js
@@ -5,7 +5,7 @@ let json = res.data
 let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*😂😂😂😂😂*", '©THE DARK KING', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*😂😂😂😂😂*", '©THE DARK KING', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^meme$/i
 handler.group = true
 handler.admin = true
