10f4918695581c3bb07ccdea88e7153632dc0f58
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:45:51 -0500

    Update lesbian.js

--- plugins/lesbian.js
+++ plugins/lesbian.js
@@ -5,7 +5,7 @@ let json = res.data
 let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*Lesbians*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*Lesbians*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(imglesbians)$/i
 handler.group = true
 handler.admin = true
