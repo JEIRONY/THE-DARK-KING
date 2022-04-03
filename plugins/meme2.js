ab719b2bde95bbf91acb0f43f1300569f9cbc206
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:34:55 -0500

    Update meme2.js

--- plugins/meme2.js
+++ plugins/meme2.js
@@ -2,9 +2,11 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/meme")
 let json = res.data
-let THE DARK KING = json.url
+let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, THE DARK KING, "*😂😂😂😂😂*", '©THE DARK KING', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(meme2)$/i
+handler.group = true
+handler.admin = true
 module.exports = handler
