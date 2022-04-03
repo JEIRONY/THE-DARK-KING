b56c8e49036544a7548a633574aae46a768dbe00
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:43:48 -0500

    Update ecchi.js

--- plugins/ecchi.js
+++ plugins/ecchi.js
@@ -5,7 +5,7 @@ let json = res.data
 let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*Sexy ❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*Sexy ❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^ecchi$/i
 handler.group = true
 handler.admin = true
