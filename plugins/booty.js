b5a55a3cf5fef411790b343ea01b69eb2b9fd8ca
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:43:17 -0500

    Update booty.js

--- plugins/booty.js
+++ plugins/booty.js
@@ -5,7 +5,7 @@ let json = res.data
 let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*Booty 🍑❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*Booty 🍑❤️‍🔥*", '©THE DARK KING', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(booty|culo|culos|culo2|culos2)$/i
 handler.rowner = true
 
