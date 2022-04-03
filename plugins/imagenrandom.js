0d38a5b95b91088a7706dfebd53563660d19b3aa
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-03 14:44:48 -0500

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -5,7 +5,7 @@ let json = res.data
 let THEDARKING = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, THE DARK KING, "*Random img 👽*", '©THE DARK KING', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, THEDARKING, "*Random img 👽*", '©THE DARK KING', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(imagenrandom)$/i
 handler.group = true
 handler.admin = true
