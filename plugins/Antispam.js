94bda77fd2d00e6cb9bc52d4b92b01abbd94fd73
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-09 23:52:40 -0500

    Update Antispam.js

--- plugins/Antispam.js
+++ plugins/Antispam.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
+            if (this.spam[m.sender].count > 5) {
                 //global.db.data.users[m.sender].banned = true
                 m.reply('TE VOY A SACAR CONCHA TU MADRE!!')
             }
@@ -20,12 +20,7 @@ export async function all(m) {
             lastspam: 0
         }
 }
-handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
-  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-   await m.reply(`*「 ANTI LINKS 」*\n*ADIOS RATA. ${await this.getName(m.sender)} POR PUTO Y POR MANDAR LINKS ....!!*`)
-    if (isAdmin) return m.reply('*Ah cagon sos retrol he? te salvas por ser admin puto*')
-    if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
+
     
      await this.groupRemove(m.chat, [m.sender])
   }
