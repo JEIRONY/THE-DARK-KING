4a5f16ebb464d8590b427e0815816eef3335b5ba
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-09 21:13:44 -0500

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -5,6 +5,7 @@ let who = m.sender
 let pp = await(await fetch('https://i.imgur.com/KVqY6z3.jpg')).buffer()
 let kai = await(await fetch('https://i.imgur.com/KVqY6z3.jpg')).buffer()
 let text = `*https://github.com/JEIRONY/THE-DARK-KING*`.trim()
+let text = `*https://www.youtube.com/channel/UCCrEOFYcEqK28CJq7EwB6rg*`.trim()
 conn.reply(m.chat, text, m, { thumbnail: kai, contextInfo: {
 mentionedJid: [who],
 externalAdReply: {
