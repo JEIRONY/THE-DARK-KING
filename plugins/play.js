1ba1688e49db171048113e876868c05714fc163e
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-09 23:24:24 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -28,12 +28,9 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (yt2 === false) throw '*Todos los servidores fallaron*'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
   await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
-📌 Titulo: ${title}
-🔗 Url: ${url}
-🗒️ Descrición: ${description}
-⏲️ Publicado: ${publishedTime}
-⌚ Duración: ${durationH}
-👁️ Vistas: ${viewH}
+*🔥 Titulo:* _${title}_
+*📂 Peso del audio:* _${filesizeF}_
+*📂 Peso del video:* _${yt2.filesizeF}_
 `.trim(), '©THE DARK KING', '🎵 AUDIO 💽 ', `.yta ${vid.url}`, '🎥 VIDEO 🎞️', `.yt ${vid.url}`)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
