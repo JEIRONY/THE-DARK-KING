2a93fb0e554066313c1ff39851b453c1c618f263
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-10 16:36:42 -0500

    Update chiste.js

--- plugins/chiste.js
+++ plugins/chiste.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`*┌────「 CHISTE 」─*\n*“${pickRandom(global.chiste)}”*\n*└────「 THE DARK KING 」─*`, m)
+  conn.reply(m.chat,`「 CHISTE 」\n*“${pickRandom(global.chiste)}”*\n「 THE DARK KING 」\n RIASE BOBO HIJUEPUTA POR ESO NO TIENE NOVI@`, m)
 }
 handler.help = ['chiste']
 
@@ -12,8 +12,11 @@ function pickRandom(list) {
 }
 
 global.chiste = [
-"¿Que le dice un fideo a otro fideo?    mi cuerpo pide salsa xd  RIASE BOBO HIJUEPUTA",
-"¿Que seria mexico sin tacos?   pues un pais destacado      RIASE BOBO HIJUEPUTA "
-
-
+"¿Que le dice un fideo a otro fideo?    mi cuerpo pide salsa ",
+"¿Que seria mexico sin tacos?   pues un pais destacado  ",
+"¿Que le dijo un globo a otro globo en un desierto?    ¡CUIDADO CON EL CACTUSSSSSSSSSS!",
+ "¿Que le dice un huevo riendose al otro ?        AJAJA CALLATE QUE ME PARTO DE RIIISA",
+"¿Qué le dice un jardinero a otro?            Disfrutemos mientras podamos"
+  
+  
 ] 
