f2038c5242c30981a6ae65a320e187a33c96ff54
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-17 18:43:37 -0500

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
         mentionedJid: conn.parseMention(teks)
       }
     })
-    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'Hey!! Hola, me presento, soy THE DARK KING, y soy un Bot para WhatsApp, una persona del grupo utilizo el comando para añadirte al grupo, pero no pude agregarte, asi que te mando la invitacion para que te agregues, te esperamos!!', jpegThumbnail ? {
+    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'Hey!! Hola, me presento, soy The Shadow Brokers - Bot, y soy un Bot para WhatsApp, una persona del grupo utilizo el comando para añadirte al grupo, pero no pude agregarte, asi que te mando la invitacion para que te agregues, te esperamos!!', jpegThumbnail ? {
       jpegThumbnail
     } : {})
   }
@@ -34,9 +34,16 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
 handler.help = ['add', '+'].map(v => v + ' número')
 handler.tags = ['admin']
 handler.command = /^(add|agregar|añadir|\+)$/i
-handler.fail = null
-handler.group = true
-handler.admin = true
+handler.owner = false
+handler.mods = false
 handler.premium = false
-handler.botAdmin = false
+handler.group = true
+handler.private = false
+
+handler.admin = true
+handler.botAdmin = true
+
+handler.fail = null
+handler.limit = false
+
 module.exports = handler
