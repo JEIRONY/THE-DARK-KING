8750c3f2dc19d78f5cbeeff08f749673aee287ce
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-28 17:59:52 -0500

    Update appm.js

--- plugins/appm.js
+++ plugins/appm.js
@@ -18,7 +18,7 @@ let menu =`
 AQUI ABAJO TE DEJO ALGUNOS LINKS DE DESCARGA ESPERO SEA DE TU AGRADO.`.trim()
 let mentionedJid = [who]
 conn.send2ButtonImg(m.chat, pp, menu, '©THE DARK KING BOT\n █║▌║││█║▌║▌║', '𝕄𝔼𝔾𝔸', `#descarga2`, '𝐀𝐏𝐊𝐀𝐃𝐌𝐈𝐍', `#descarga`, m, false, { contextInfo: { mentionedJid }})   
-type: 'audioMessage', 
+type: 'Message', 
 ptt: true 
 })
 handler.command = /^(app|apk|APP|APK|minecraft)$/i
