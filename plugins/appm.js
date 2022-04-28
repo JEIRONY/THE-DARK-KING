19208469c667e214a3e2fcc24e0d90823a8d3dca
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-04-28 17:58:21 -0500

    Update appm.js

--- plugins/appm.js
+++ plugins/appm.js
@@ -18,10 +18,9 @@ let menu =`
 AQUI ABAJO TE DEJO ALGUNOS LINKS DE DESCARGA ESPERO SEA DE TU AGRADO.`.trim()
 let mentionedJid = [who]
 conn.send2ButtonImg(m.chat, pp, menu, '©THE DARK KING BOT\n █║▌║││█║▌║▌║', '𝕄𝔼𝔾𝔸', `#descarga2`, '𝐀𝐏𝐊𝐀𝐃𝐌𝐈𝐍', `#descarga`, m, false, { contextInfo: { mentionedJid }})   
-await await await await await await conn.sendFile(m.chat, vn, 'ora.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
-})}
+})
 handler.command = /^(app|apk|APP|APK|minecraft)$/i
 handler.fail = null
 module.exports = handler
